3aacce8f3267ba4757ea19b2e5aabcb29a4f2309
SamiraFreitas <samfreitascomp@gmail.com>
2021-11-10 18:46:01 -0300

    Modal-Forms

--- 02-primeiro-app-react/src/App.tsx
+++ 02-primeiro-app-react/src/App.tsx
@@ -1,12 +1,32 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { useState } from 'react';
+import Modal from 'react-modal'
 import { GlobalStyle } from './styles/global';
-
+Modal.setAppElement('#root');
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+
+    function handleCloserNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
+
+      <Modal 
+      isOpen={isNewTransactionModalOpen} 
+      onRequestClose={handleCloserNewTransactionModal}
+      >
+       <h2>Cadastrar Transação</h2>
+       </Modal>
       <GlobalStyle/>
     </>
   );
